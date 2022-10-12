1fe53fdf3628d0d15dbbc3e36b1a1b6908fbd9da
zydnrbrn <zidanreborn@gmail.com>
2022-10-12 15:47:50 +0700

    updates footer

--- resources/js/Pages/Components/Footer.jsx
+++ resources/js/Pages/Components/Footer.jsx
@@ -1,12 +1,32 @@
+import { Link } from "@inertiajs/inertia-react"
+
 export default function Footer() {
     return (
         <>
-        <div className="w-full h-24 bg-main-red">  
-        <h1 className='sm:text-2xl
-            text-xl font-bold font-righteous text-white
-            '>
-                Nectify
-                </h1></div>
+        <div className="flex text-white
+        w-full bg-main-red">  
+       
+                © 2022 Nectify.All Rights Reserved.
+
+                <ul className="lg:mt-0 mx-[300px]
+                        items-center space-y-8 md:flex md:space-x-3 md:items-end sm:my-3">
+                            <li className="sm:text-lg md:text-base
+                            text-white hover:text-black  hover:rounded-sm
+                            ">
+                                <Link href='/'>Home</Link>
+                            </li>
+                            <li className="sm:text-lg  md:text-base
+                           text-white hover:text-black hover:rounded-sm
+                            ">
+                               <Link href='/chatify'>Chats</Link>
+                            </li>
+                            <li className="sm:text-lg  md:text-base
+                           text-white hover:text-black hover:rounded-sm
+                            ">
+                           <Link href='/post'>Post</Link>
+                            </li>
+                        </ul>
+                </div>
         </>
     )
 }
