418f815c93c370408a194bd17dc3876601daee63
zydnrbrn <zidanreborn@gmail.com>
2022-10-13 08:27:25 +0700

    push to heroku

--- resources/js/Pages/Components/Description.jsx
+++ resources/js/Pages/Components/Description.jsx
@@ -1,9 +1,38 @@
 export default function() {
     return(
         <>
-        <div className="mx-10 mt-16 mb-16">
+        <div className="mx-10 mt-16 mb-16 bg-main-red">
 
- ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptates odio inventore facilis cupiditate enim a quaerat quam natus eaque. Nostrum est dolores odio rerum, consequuntur ducimus ea ab sint! Excepturi ratione repudiandae quos vel! Harum nesciunt a ullam laborum fugiat! Ab asperiores sequi nemo, numquam nesciunt vitae quos illo ex eaque! Error officiis necessitatibus laboriosam distinctio fugiat adipisci expedita nulla veritatis? Nisi quaerat corrupti delectus et libero? Suscipit accusantium nemo, debitis praesentium natus modi repellat necessitatibus quam nostrum facilis perspiciatis dolor ullam error quia mollitia corporis ab eveniet, quod esse. Consequatur, recusandae? Animi eum distinctio illum cumque aspernatur ex?
+        {/* isi disini */}
+
+
+        1
+
+
+
+        {/* sampai sini */}
+        </div>
+        <div className="mx-10 mt-16 mb-16 bg-main-red">
+
+        {/* isi disini */}
+
+
+
+2
+
+
+        {/* sampai sini */}
+        </div>
+        <div className="mx-10 mt-16 mb-16 bg-main-red">
+
+        {/* isi disini */}
+
+
+
+
+3
+
+        {/* sampai sini */}
         </div>
         </>
 
